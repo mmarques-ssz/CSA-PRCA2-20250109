486c6444417979c7b1108667ecfdce31bf3309cc
mmarques-ssz <37246763+mmarques-ssz@users.noreply.github.com>
2025-01-09 20:18:48 -0300

    Add files via upload

---
+++ WFParalela/WFParalela/Paralela.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using System.Runtime.InteropServices; // tem que colocar
+
+namespace WFParalela
+{
+    class Paralela
+    {
+        // Escreve um byte no endereço
+        [DllImport("Inpout32.dll", EntryPoint = "Out32")]
+        public static extern void Escrever(int endereco, byte valor);
+    }
+}
